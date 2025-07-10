203115da2c5e274617853d5d30bb9ed27c1eb3f9
mehulraj <mailtomehulraj@gmail.com>
2025-07-10 15:41:27 +0530

    assigned roles changed

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const ticketController = require('../controllers/ticketController');
 const { protect, authorizeRoles } = require("../middlewares/authMiddleware");
 
-// USER
+// user , operation, technical, admin
 router.post('/create', protect, authorizeRoles("user"), ticketController.createTicket);
 router.get("/my-tickets", protect, authorizeRoles("user", "technical", "operation"), ticketController.getUserTickets);
-router.get("/count", protect, authorizeRoles("user", "technical", "operation"), ticketController.ticketCounts)
-// OPERATION / ADMIN
-router.get("/getalltickets", protect, authorizeRoles("operation", "admin"), ticketController.getAllTickets);
+router.get("/count", protect, authorizeRoles("user", "technical", "operation", "admin"), ticketController.ticketCounts)
+// operarion
+router.get("/getalltickets", protect, authorizeRoles("operation"), ticketController.getAllTickets);
 router.post("/op_approvals", protect, authorizeRoles("operation"), ticketController.op_approvals);
 
-// TECHNICAL / OPERATION / ADMIN
-router.put("/resolve/:id", protect, authorizeRoles("technical", "operation", "admin"), ticketController.resolveTicket);
+// technical, operation
+router.put("/resolve/:id", protect, authorizeRoles("technical", "operation"), ticketController.resolveTicket);
 
 module.exports = router;
