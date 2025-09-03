08a460e625e35bdb63b3fab779dbae6265b94bda
mehul <mailtomehulraj@gmail.com>
2025-09-03 23:42:21 +0530

    home route added

--- backend/app.js
+++ backend/app.js
@@ -29,9 +29,11 @@ app.use(express.json());
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "HOME ROUTE" })
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/tickets", ticketRoutes)
-
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
